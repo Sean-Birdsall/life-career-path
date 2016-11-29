dfcdebbdf1ef9b628738be07f0052d6a5c613650
Student <student@Admins-Mac-mini-24.local>
2016-11-29 16:44:46 -0700

    Update from student machine

--- server.js
+++ server.js
@@ -2,7 +2,21 @@ var express = require('express');
 
 var app = express();
 
+var bodyParser = require('body-parser');
+
+// CREATE APPLICATION/X-WWW-FORM-URLENCODED PARSER
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 app.use(express.static('public'));
+
+app.post('/sign-up', urlencodedParser, (req, res) => {
+  // PREPARE OUTPUT IN JSON FORMAT
+  console.log(req.body);
+})
+
+app.get('/log-in', (req, res) => {
+  console.log(req.query);
+})
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('This shit is working');
